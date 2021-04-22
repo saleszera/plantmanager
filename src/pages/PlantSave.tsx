daac460363e3d5b310527cb9063e960717852d6f
saleszera <crowzcoleslaw@gmail.com>
2021-04-22 13:18:04 -0300

    adiciona componente de card para listagem de plantas na tela de minhas plantas

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -6,7 +6,6 @@ import {
   Alert,
   View,
   Image,
-  ScrollView,
   Platform,
   TouchableOpacity,
 } from 'react-native';
@@ -14,11 +13,11 @@ import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { SvgFromUri } from 'react-native-svg';
 import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 import { isBefore, format } from 'date-fns';
-import waterdrop from '../assets/waterdrop.png';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 import { PlantProps, savePlant } from '../libs/storage';
+import waterdrop from '../assets/waterdrop.png';
 
 interface Params {
   plant: PlantProps;
